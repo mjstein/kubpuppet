33e4c6c053a5e613b70440cf61fe8409699fa182
Stein <mst43@ostml0231493.bskyb.com>
2017-01-06 19:52:04 +0000

    fsake

--- manifest/kubmas.pp
+++ manifest/kubmas.pp
@@ -1,13 +1,13 @@
 class { 'etcd':
   ensure                     => 'latest',
-  listen_client_urls    => 'http://127.0.0.1:2379',
+  listen_client_urls    => 'http://0.0.0.0:2379',
 }
 etcd_key { '/atomic.io/network/config': value => '{ "Network": "10.100.5.0/24" }' }
 class { 'kubernetes::master::apiserver':
   allow_privileged => true,
   service_cluster_ip_range => '10.100.5.0/24',
   etcd_servers => "http://127.0.0.1:2379",
-#  etcd_prefix => '/atomic.io/network/config'
+  etcd_prefix => '/atomic.io/network/config'
 }
  class { 'kubernetes::master::scheduler':
   master => 'http://127.0.0.1:8080',
@@ -17,7 +17,7 @@ class { 'kubernetes::master::apiserver':
 }
 
 class { 'flannel':
-  etcd_endpoints => "http://127.0.0.1:2379",
-#  etcd_prefix    => '/atomic.io/network/config',
+  etcd_endpoints => "http://0.0.0.0:2379",
+  etcd_prefix    => '/atomic.io/network/config',
 }
 
