8950fab762ca960e73d7a7bd71aa63f19e34396b
Stein <mst43@ostml0231493.bskyb.com>
2017-01-06 19:29:11 +0000

    add etcd

---
+++ manifest/kubmas.pp
@@ -0,0 +1,23 @@
+class { 'etcd':
+  ensure                     => 'latest',
+  listen_client_urls    => 'http://127.0.0.1:2379',
+}
+etcd_key { '/atomic.io/network/config': value => '{ "Network": "10.100.5.0/24" }' }
+class { 'kubernetes::master::apiserver':
+  allow_privileged => true,
+  service_cluster_ip_range => '10.100.5.0/24',
+  etcd_servers => "http://127.0.0.1:2379",
+#  etcd_prefix => '/atomic.io/network/config'
+}
+ class { 'kubernetes::master::scheduler':
+  master => 'http://127.0.0.1:8080',
+}
+ class { 'kubernetes::node::kube_proxy':
+  master => 'http://127.0.0.1:8080',
+}
+
+class { 'flannel':
+  etcd_endpoints => "http://127.0.0.1:2379",
+#  etcd_prefix    => '/atomic.io/network/config',
+}
+
