9db23668d4eb3c2697860790195b8a89f683837a
mike@stein <mike@stein>
2017-01-10 16:06:54 +0000

    added users and keys

--- manifest/site.pp
+++ manifest/site.pp
@@ -17,6 +17,6 @@ ec2_instance { 'kubnode':
   image_id          => 'ami-7abd0209', # you need to select your own AMI
   key_name          => 'kubkey',
   instance_type     => 't2.medium',
-  private_ip_address => '172.31.1.1',
+  private_ip_address => '172.31.1.2',
   user_data         => template('aws_scripts/init.sh.erb'), 
 }
