6ab2241a8c69b000b9ed75f97118a6f9b1ff1c3a
mike@stein <mike@stein>
2017-01-10 13:18:22 +0000

    fix errors

--- modules/kubernetes/manifests/minion.pp
+++ modules/kubernetes/manifests/minion.pp
@@ -1,6 +1,6 @@
 class kubernetes::minion($master_name=undef, $minion_name=undef, $alternate_flannel_interface_bind = false){
   validate_string($master_name,$minion_name)
-  if not defined(class['kubernetes']){
+  if not defined('kubernetes'){
     class {'kubernetes':
       master_name =>  $master_name,
       minion_name =>  $minion_name,
