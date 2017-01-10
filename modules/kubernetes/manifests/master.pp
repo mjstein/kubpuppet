ea566cbfd8c3fe903d29d64dc752422a22781976
mike@stein <mike@stein>
2017-01-10 18:49:18 +0000

    correct ordering

--- modules/kubernetes/manifests/master.pp
+++ modules/kubernetes/manifests/master.pp
@@ -57,8 +57,7 @@ class kubernetes::master($master_name = undef, $minion_name = undef,$alternate_f
       minion_name      => $minion_name, #can be hostname if dns setup
       master_is_minion =>   true,
     }
-    contain 'kubernetes'
 
     }
-  }
+  
 }
