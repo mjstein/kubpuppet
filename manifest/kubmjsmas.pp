f2df483cd664c45069b95c1241c24a0901dce23f
mike@stein <mike@stein>
2017-01-10 13:41:33 +0000

    fix errors

--- manifest/kubmjsmas.pp
+++ manifest/kubmjsmas.pp
@@ -1,6 +1,6 @@
     class {'kubernetes::master':
       master_name => '172.31.1.1', #can be hostname if dns setup
-      minion_name => ['172.31.1.2'], #can be hostname if dns setup
+      minion_name => ['172.31.1.1','172.31.1.2'], #can be hostname if dns setup
     }
 
     class {'kubernetes::minion':
